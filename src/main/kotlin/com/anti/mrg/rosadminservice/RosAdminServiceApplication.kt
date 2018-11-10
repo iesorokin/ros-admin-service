842bd71e6cef3811e0382fc2be679a7dadf0c05d
Ivan <Ivan.Sorokin@leroymerlin.ru>
2018-11-10 21:21:38 +0300

    first init

---
+++ src/main/kotlin/com/anti/mrg/rosadminservice/RosAdminServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.anti.mrg.rosadminservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RosAdminServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<RosAdminServiceApplication>(*args)
+}
